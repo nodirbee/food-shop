b5889410bef85f6530b38161bb5e95dec703ca42
nodirbee <nodirbekigamberdiev005@gmail.com>
2024-03-23 01:16:39 +0200

    continuation

---
+++ js.js
@@ -0,0 +1,36 @@
+const prroducs = document.querySelector('.prroducs')
+
+
+
+const data = [
+  {
+    id: 1,
+    img: "./img/item_01-480x480 1.png",
+    name: "Cheeseburger With Salad",
+    price: 1800,
+  },
+  {
+    id: 2,
+    img: "./img/Burger.png",
+    name: "Beef Burger",
+    price: 1500,
+  },
+  {
+    id: 3,
+    img: "./img/item_01-480x480 1 (1).png",
+    name: "Royel Cheeseburger",
+    price: 1600,
+  },
+  {
+    id: 4,
+    img: "./img/item_01-480x480 1 (2).png",
+    name: "Black Gambugrer",
+    price: 1400,
+  },
+  {
+    id: 5,
+    img: "./img/item_01-480x480 1 (3).png",
+    name: "Chicken Burger",
+    price: 1500,
+  },
+];
