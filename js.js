a6667d033478eedb64810f2b45a988c633075886
nodirbee <nodirbekigamberdiev005@gmail.com>
2024-04-18 16:55:33 +0200

    davomi

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 const prroducs = document.querySelector('.prroducs');
 const openbasket = document.querySelector('#openbasket') ;
 const openbasketmodal = document.querySelector('.openbasketmodal');
+const tbody = document.querySelector('tbody');
 
 const data = [
   {
@@ -8,30 +9,35 @@ const data = [
     img: "./img/item_01-480x480 1.png",
     name: "Cheeseburger With Salad",
     price: 18,
+    count:1,
   },
   {
     id: 2,
     img: "./img/Burger.png",
     name: "Beef Burger",
     price: 15,
+    count:1,
   },
   {
     id: 3,
     img: "./img/item_01-480x480 1 (1).png",
     name: "Royel Cheeseburger",
     price: 16,
+    count:1,
   },
   {
     id: 4,
     img: "./img/item_01-480x480 1 (2).png",
     name: "Black Gambugrer",
     price: 14,
+    count:1,
   },
   {
     id: 5,
     img: "./img/item_01-480x480 1 (3).png",
     name: "Chicken Burger",
     price: 15,
+    count:1,
   },
 ];
 
@@ -49,7 +55,8 @@ data.map((item, index) => {
     <div class="card">
     <img src="${item.img}">
     <p>${item.name}</p>
-    <span>$${item.price}</span>
+    <span>$${item.price}</sp
+    count:1,an>
     <button onclick="addcard(${index})"  >Add to Cart</button>
     </div>
     
@@ -76,24 +83,21 @@ else{
 
 basket.map((item)=>{
 
-openbasketmodal.innerHTML+=`
-
-<div class="basketbox">
-<span>${item.id}</span>
-<img src="${item.img}" alt="">
-<p>${item.nami}</p>
-<p>${item.price}</p>
-<div>
-    <button>x</button>
-</div>
-
-</div>
-
-`;
+  tbody.innerHTML+=`
+  <tr>
+  <td>${item.id}</td>
+  <td> <img src="${item.img}" /> </td>
+  <td>${item.name}</td>
+  <td>${item.price}</td>
+  <td id="count-controller"> <button>-</button>   ${item.count} <button>+</button>     </td>
+  <td> <button id="del">del</button>  </td>
+</tr>
+  
+  
+  
+  
+  `
 })
-
-
-
 
 
 
